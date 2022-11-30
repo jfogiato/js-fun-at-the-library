e97063e54a3e58fb8835be6a0754ab6cfbdf1e19
Joe Fogiato <joe.fogiato@gmail.com>
2022-11-30 18:15:04 -0500

    Update file with testing solutions

--- src/book.js
+++ src/book.js
@@ -1,12 +1,45 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
+  }
+}
+
+function saveReview(review, array) {
+  if (array.includes(review)) {
+    return array
+  } else {
+    return array.push(review)
+  }
+}
+
+function calculatePageCount(title) {
+  return title.length * 20
+}
+
+function writeBook(title, character, genre) {
+  return {
+    title: title,
+    mainCharacter: character,
+    pageCount: calculatePageCount(title),
+    genre: genre
+  }
+}
+
+function editBook(book) {
+  return book.pageCount *= .75
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
