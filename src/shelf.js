d0bab07d74cbf14a594312b82654ac62f56467f6
Joe Fogiato <joe.fogiato@gmail.com>
2022-11-30 20:57:58 -0500

    Update file with completed tests & functions

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,41 @@
+function shelfBook(title, shelf) {
+    if (shelf.length < 3) {
+      return shelf.unshift(title)
+    }
+}
+
+function unshelfBook(name, shelf) {
+ for (i = 0; i < shelf.length; i++) {
+  if (name === shelf[i].title) {
+    return shelf.splice(i,1)
+  }
+ }
+}
+
+function listTitles(shelf) {
+  var newShelf = []
+  for (i = 0; i < shelf.length; i++) {
+    if (i < shelf.length - 1) {
+      newShelf += `${shelf[i].title}, `
+    } else {
+      newShelf += `${shelf[i].title}`
+    }
+  }
+  return newShelf
+}
+
+function searchShelf(shelf, name) {
+  for (i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === name) {
+      return true
+    }
+  }
+return false
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
