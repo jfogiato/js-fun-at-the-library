0b2ac76004d1661cad2b40936884928da8df6033
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-02 18:56:11 -0500

    Update librarian file with solutions

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,17 @@
 class Librarian {
-  constructor() {
+  constructor(name, library) {
+    this.name = name
+    this.library = library
+  }
+  greetPatron(name, bool) {
+    return (bool ?  `Good morning, ${name}!` : `Hello, ${name}!`)
+  }
+  findBook(title) {
+   
+    for (i = 0; i < library.shelves[which shelf to check].length; i++) {
+\
     
+    // return (library.shelves ? `Yes, we have ${title}`: `Sorry we do not have ${title}`)
   }
 }
 
