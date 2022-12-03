488976505bc97414acc605c6771d8e31ff89c6f6
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-03 14:55:19 -0500

    Update librarian files with working functions and passing tests

--- src/librarian.js
+++ src/librarian.js
@@ -6,13 +6,33 @@ class Librarian {
   greetPatron(name, bool) {
     return (bool ?  `Good morning, ${name}!` : `Hello, ${name}!`)
   }
-  findBook(title) {
-   
-    for (i = 0; i < library.shelves[which shelf to check].length; i++) {
-\
-    
-    // return (library.shelves ? `Yes, we have ${title}`: `Sorry we do not have ${title}`)
+  findBook(bookString) {
+    var genreArray = Object.keys(this.library.shelves)
+    for (var i = 0; i < genreArray.length; i++) {
+      var genre = Object.keys(this.library.shelves)[i]
+      if (this.library.shelves[genre][i].title === bookString) {
+        this.library.shelves[genre].splice(i, 1)
+        return `Yes, we have ${bookString}`
+      } else {
+        return `Sorry, we do not have ${bookString}`
+      }
+    }
+  }
+  calculateLateFee(days) {
+    return Math.ceil(days * .25)
   }
 }
+
+// ***createLibrary & checkoutBook FUNCTIONS FROM LIBRARY.JS FOR DATA STRUCTURE REFERENCE***
+// function createLibrary(name) {
+//   return {
+//     name: name,
+//     shelves: {
+//       fantasy: [{title: 'The Fifth Season', genre: 'fantasy'}],
+//       fiction: [],
+//       nonFiction: []
+//     }
+//   }
+// }
 
 module.exports = Librarian;
