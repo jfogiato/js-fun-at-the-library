1606e0196adc674185a3937d7235da96dbb71c63
Joe Fogiato <joe.fogiato@gmail.com>
2022-12-03 15:07:48 -0500

    Update logic of findBook to be able to iterate through untlimited amount of books on each shelf

--- src/librarian.js
+++ src/librarian.js
@@ -7,15 +7,17 @@ class Librarian {
     return (bool ?  `Good morning, ${name}!` : `Hello, ${name}!`)
   }
   findBook(bookString) {
-    var genreArray = Object.keys(this.library.shelves)
+    var genreArray = Object.keys(this.library.shelves) // returns array of shelves labeled by genre to be used in the loop as the number of times to loop through and check each shelf
     for (var i = 0; i < genreArray.length; i++) {
       var genre = Object.keys(this.library.shelves)[i]
       var shelf = this.library.shelves[genre]
-      if (shelf[i].title === bookString) {
-        shelf.splice(i, 1)
-        return `Yes, we have ${bookString}`
+      for (var j = 0; j < shelf.length; j++) {
+        if (shelf[i].title === bookString) {
+          shelf.splice(i, 1)
+          return `Yes, we have ${bookString}`
       } else {
         return `Sorry, we do not have ${bookString}`
+      }
       }
     }
   }
